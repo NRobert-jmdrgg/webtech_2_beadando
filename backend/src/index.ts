3ee64455222799298f07a9fac6383eabec79a8aa
Róbert Nagy <rob.iskola1@gmail.com>
2023-03-15 19:27:59 +0100

    mongoose

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,23 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import { MongoClient } from 'mongodb';
+import { connect } from 'mongoose';
+import { Schema } from 'inspector';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-const mongo_client = new MongoClient(process.env.MONGO_URI!);
-mongo_client
-  .connect()
+connect(process.env.MONGO_URI!)
   .then(() => {
     console.log('mongodb connected...');
+
+    app.get('/', async (req: Request, res: Response) => {
+      res.status(200).send('');
+    });
+
+    app.listen(port, () => {
+      console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    });
   })
   .catch((error) => console.error(error));
-
-app.get('/', async (req: Request, res: Response) => {
-  res.status(200).send('');
-});
-
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
