4c09aae2b2fc08ca805314e88397e0d451ff3bcf
Róbert Nagy <rob.iskola1@gmail.com>
2023-03-24 22:49:46 +0100

    user page

--- frontend/src/app/components/private/user-page/user-page.component.ts
+++ frontend/src/app/components/private/user-page/user-page.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '@models/user';
+import { AuthService } from '@services/auth/auth.service';
 
 @Component({
   selector: 'app-user-page',
   templateUrl: './user-page.component.html',
   styleUrls: ['./user-page.component.css'],
 })
-export class UserPageComponent {}
+export class UserPageComponent implements OnInit {
+  user!: User | null;
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.currentUser$.subscribe({
+      next: (user) => (this.user = user),
+      error: (error) => console.error(error),
+    });
+  }
+}
