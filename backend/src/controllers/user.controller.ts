9741d2f7853833f92fd770286f56fd4ac645fd11
Róbert Nagy <rob.iskola1@gmail.com>
2023-03-24 20:24:34 +0100

    check for email and username + getusename

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -6,6 +6,16 @@ interface UserRequest extends Request {
   user?: IUser;
 }
 
+const checkIfExsits = async (prop: { [x: string]: string }) => {
+  try {
+    const res = await User.find(prop);
+    return res && res.length;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
+
 export const getAllUsers = async (req: Request, res: Response) => {
   try {
     res.status(200).send(await User.find({}));
@@ -18,6 +28,15 @@ export const getAllUsers = async (req: Request, res: Response) => {
 export const addUser = async (req: UserRequest, res: Response) => {
   try {
     const { email, password, name, phone, firstName, lastName } = req.body.user;
+
+    if (await checkIfExsits({ email })) {
+      return res.status(400).send({ message: 'email already in use!' });
+    }
+
+    if (await checkIfExsits({ name })) {
+      return res.status(400).send({ message: 'username already in use!' });
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({
       email,
@@ -39,10 +58,14 @@ export const addUser = async (req: UserRequest, res: Response) => {
   }
 };
 
-export const getUserById = async (req: Request, res: Response) => {
+export const getUserNameById = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
-    res.status(200).send(await User.findById(id));
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).send({ message: 'User not found' });
+    }
+    return res.status(200).send({ name: user.name });
   } catch (error) {
     console.error(error);
     res.status(500).send({ message: 'An error occurred' });
