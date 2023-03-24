1f93aa03ca78df0f0e026e8aa2e5e9ffb7e62fb4
Róbert Nagy <rob.iskola1@gmail.com>
2023-03-24 10:06:58 +0100

    query params

--- frontend/src/app/components/private/registry/registry.component.ts
+++ frontend/src/app/components/private/registry/registry.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from '@services/product/product.service';
 
 /**
@@ -20,7 +20,11 @@ export class RegistryComponent implements OnInit {
 
   dataSource: Product[];
 
-  constructor(private router: Router, private productService: ProductService) {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductService
+  ) {
     this.dataSource = [];
     this.currentPage = 0;
     this.pageSize = 10;
@@ -30,17 +34,27 @@ export class RegistryComponent implements OnInit {
     this.router.navigate(['/product', id]);
   }
 
+  setUrlQueryParams(index: string, size: string) {
+    this.router.navigateByUrl(`/registry?page=${index}&size=${size}`);
+  }
+
   loadProducts(event: any) {
     console.log(JSON.stringify(event, null, 2));
     const { pageIndex, pageSize } = event;
     this.productService.getProducts(pageIndex * pageSize, pageSize).subscribe({
       next: (products) => (this.dataSource = products),
-
       error: (error) => console.error(error),
     });
+
+    this.setUrlQueryParams(pageIndex, pageSize);
   }
 
   ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      this.currentPage = params['page'] ?? 0;
+      this.pageSize = params['size'] ?? 10;
+    });
+
     this.productService.getProducts(0, this.pageSize).subscribe({
       next: (products) => (this.dataSource = products),
       error: (error) => console.error(error),
