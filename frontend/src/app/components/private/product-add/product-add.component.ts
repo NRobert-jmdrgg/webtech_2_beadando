56e2c4a4ac1433f41f4c6252eb70ef8eb75e80fc
Róbert Nagy <rob.iskola1@gmail.com>
2023-03-24 10:06:37 +0100

    product add component

--- frontend/src/app/components/private/product-add/product-add.component.ts
+++ frontend/src/app/components/private/product-add/product-add.component.ts
@@ -1,8 +1,34 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ProductService } from '@services/product/product.service';
+import { FormErrorStateMatcher } from '@utils/formatStateMatcher';
 
 @Component({
   selector: 'app-product-add',
   templateUrl: './product-add.component.html',
   styleUrls: ['./product-add.component.css'],
 })
-export class ProductAddComponent {}
+export class ProductAddComponent {
+  productAddForm = new FormGroup({
+    name: new FormControl(null, [Validators.required]),
+    brand: new FormControl(null, [Validators.required]),
+    price: new FormControl(null, [Validators.required]),
+    category: new FormControl(null, [Validators.required]),
+  });
+  matcher = new FormErrorStateMatcher();
+
+  constructor(private router: Router, private productService: ProductService) {}
+
+  onSubmit() {
+    this.productService.addProduct(this.productAddForm.value).subscribe({
+      next: (res) => console.log(res),
+      error: (error) => console.error(error),
+    });
+    this.router.navigate(['/registry']);
+  }
+
+  cancel() {
+    this.router.navigate(['/registry']);
+  }
+}
