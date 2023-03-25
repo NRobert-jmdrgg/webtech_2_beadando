d0eda0351b873f5c105e0462c14f068e8f0927e1
Róbert Nagy <rob.iskola1@gmail.com>
2023-03-25 16:10:12 +0100

    remove protected route

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,12 +1,7 @@
 import express from 'express';
 import { signin } from '../controllers/auth.controller';
-import { verifyToken } from '../middleware/verifyJWT';
 
 const router = express.Router();
 
 router.post('/signin', signin);
-router.post('/', verifyToken, (req, res) => {
-  console.log('hello world');
-  res.send(req.body.verified);
-});
 export default router;
