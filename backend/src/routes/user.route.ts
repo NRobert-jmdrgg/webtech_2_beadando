ea71c3e0d62bdbf108847858e03ed346cc2c4a43
Róbert Nagy <rob.iskola1@gmail.com>
2023-03-25 16:19:51 +0100

    remove routes

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,12 +1,10 @@
 import express from 'express';
-import { deleteUser, getAllUsers, getUserNameById, updateUser, addUser } from '../controllers/user.controller';
+import { getUserNameById, addUser } from '../controllers/user.controller';
+import { verifyToken } from '../middleware/verifyJWT';
 
 const router = express.Router();
 
-router.get('/', getAllUsers);
 router.post('/add', addUser);
-router.get('/:id', getUserNameById);
-router.put('/:id', updateUser);
-router.delete('/:id', deleteUser);
+router.get('/:id', [verifyToken], getUserNameById);
 
 export default router;
